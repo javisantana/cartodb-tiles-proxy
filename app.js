2078e3b2636e9f93c9968ca99d412351558b8df8
javi <qualopec@gmail.com>
2013-02-18 12:29:31 +0100

    included tempalte

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 
 var httpProxy = require('http-proxy');
 var querystring = require('querystring')
+var url  = require('url');
 
 var argv = process.argv;
 
-if(argv.length != 4) {
-  console.log('usage: node app.js cartodb_host api_key');
-  console.log('\nexample:');
-  console.log('    node app.js examples.cartodb.com 1af665a5f5d5D');
-  process.exit();
+var env = process.env;
+if(env.HOST && env.API_KEY && env.PORT) {
+  //running on heroku
+  var host = env.HOST;
+  var api_key = env.API_KEY;
+  var port = env.PORT;
+} else {
+  // command line
+  if(argv.length < 4) {
+    console.log('usage: node app.js cartodb_host api_key');
+    console.log('\nexample:');
+    console.log('    node app.js examples.cartodb.com 1af665a5f5d5D');
+    process.exit();
+  }
+  var host = argv[2];
+  var api_key = argv[3];
+  var port = 9090;
+  if(argv.length > 4) {
+    port = parseInt(argv[4], 10);
+  }
 }
 
-var host = argv[2];
-var api_key = argv[3];
 
 var config = {}
 try {
@@ -28,19 +42,60 @@ function get_params_for_url(url) {
   return [];
 }
 
+function escapeSql(val) {
+  val.replace(/[\0\n\r\b\t\\\'\"\x1a]/g, function (s) {
+    switch (s) {
+      case "\0":
+        return "\\0";
+      case "\n":
+        return "\\n";
+      case "\r":
+        return "\\r";
+      case "\b":
+        return "\\b";
+      case "\t":
+        return "\\t";
+      case "\x1a":
+        return "\\Z";
+      default:
+        return "\\" + s;
+    }
+  });
+
+  return val;
+}
+
+function template(s, vars){
+ for(var p in vars) {
+   s = s.replace(new RegExp('{' + p + '}','g'), escapeSql(vars[p]));
+ }
+ return s;
+}
 
 httpProxy.createServer(function (req, res, proxy) {
-  req.url += (~req.url.indexOf('?') ? '&' : '?') + "api_key=" + api_key;
-  var params = get_params_for_url(req.url);
+  var vars = url.parse(req.url, true).query
+  console.log(vars);
+  // remove params
+  req.url = req.url.split('?')[0]
+  req.url += "?api_key=" + api_key;
+  var paramsTmpl = get_params_for_url(req.url);
+  var params = {};
+  for(var p in paramsTmpl) {
+    params[p] = template(paramsTmpl[p], vars);
+  }
   req.url += "&" + querystring.stringify(params);
-  //req.url = encodeURIComponent(req.url);
+
   req.headers['Host'] = host;
   req.method = 'GET'
   req.headers['Content-Length'] = 0;
   console.log(req.url);
+
   proxy.proxyRequest(req, res, {
     host: host,
     port: 80 
   });
-}).listen(9090);
+
+}).listen(port);
+
+console.log("listening on port " + port);
 
